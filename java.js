9a5d5cc58207809b3516f105625afe3a71f90a69
mdrahmanali <152678646+mdrahmanali@users.noreply.github.com>
2025-01-30 00:04:51 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".text",{
+    strings:["My First Project...","Frontend Developer...", "Coder...", "Gamer..." ],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
